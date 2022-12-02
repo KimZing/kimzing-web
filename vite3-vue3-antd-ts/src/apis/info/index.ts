9d0e1a6304ead9314299a4dcfa569211b1a30234
kimzing <kimzing@163.com>
2022-12-02 17:44:29 +0800

    基础格式修改

--- vite3-vue3-antd-ts/src/apis/info/index.ts
+++ vite3-vue3-antd-ts/src/apis/info/index.ts
@@ -2,4 +2,4 @@ import http from '@/utils/http'
 import { Info } from '@/models/info'
 
 // 获取基础信息
-export const getInfo = () => http.get<Info>('/info', 1)
+export const getInfo = () => http.get<Info>('/info')
