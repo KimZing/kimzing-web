b8e3e570e27feb0662681f4d049404d6091ea255
kimzing <kimzing@163.com>
2022-12-04 19:15:09 +0800

    添加less

--- vite3-vue3-antd-ts/src/layout/content/content.vue
+++ vite3-vue3-antd-ts/src/layout/content/content.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="content-container">
     <BackTop id="content-container" />
-    <div v-for="i in 100" :key="i"> Con </div>
+    <div v-for="i in 100" :key="i" class="testDiv"> Con </div>
   </div>
 </template>
 
@@ -9,11 +9,15 @@
 import BackTop from '@/components/backtop/BackTop.vue'
 </script>
 
-<style scoped>
+<style scoped lang="less">
+@height: 100%;
 /*需要将所有父级 html body app layout的高度设置为100%，此处才会做到填充效果*/
 #content-container {
-  height: 100%;
-  max-height: 100%;
+  height: @height;
+  max-height: @height;
   overflow: auto;
+  .testDiv {
+    background-color: aqua;
+  }
 }
 </style>
