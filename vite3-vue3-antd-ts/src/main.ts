42ea1474da27d40f5ab5766de40cc99acc6a88b5
kimzing <kimzing@163.com>
2022-12-04 19:38:52 +0800

    添加动态主题色

--- vite3-vue3-antd-ts/src/main.ts
+++ vite3-vue3-antd-ts/src/main.ts
@@ -7,4 +7,4 @@ import mock from '@/plugins/mock'
 import router from '@/router'
 import { store, key } from '@/store'
 
-createApp(App).use(Antd).use(router).use(store, key).use(mock).mount('#app')
+createApp(App).use(Antd, '#25b864').use(router).use(store, key).use(mock).mount('#app')
--- vite3-vue3-antd-ts/src/plugins/antd/index.ts
+++ vite3-vue3-antd-ts/src/plugins/antd/index.ts
@@ -1,9 +1,16 @@
 import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/antd.variable.min.css'
 import { App } from 'vue'
+import { ConfigProvider } from 'ant-design-vue'
 
-export default{
-  install (app: App) {
+export default {
+  install(app: App, color: string) {
+    //  设置主题色, 可动态更改
+    ConfigProvider.config({
+      theme: {
+        primaryColor: color
+      }
+    })
     app.use(Antd)
   }
 }
