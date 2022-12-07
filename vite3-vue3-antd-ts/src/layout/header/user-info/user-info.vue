725afea321094e64df9136ee82e5d7fa259518e8
kimzing <kimzing@163.com>
2022-12-07 19:27:03 +0800

    去除prettier，完善eslint规则

--- vite3-vue3-antd-ts/src/layout/header/user-info/user-info.vue
+++ vite3-vue3-antd-ts/src/layout/header/user-info/user-info.vue
@@ -1,5 +1,7 @@
 <template>
-  <a-avatar size="large" style="color: #f56a00; background-color: #fde3cf"> Admin </a-avatar>
+  <a-avatar size="large" style="color: #f56a00; background-color: #fde3cf">
+    Admin
+  </a-avatar>
 </template>
 
 <script setup lang="ts"></script>
